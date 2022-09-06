b60a50f1e0d7d113831d61c625d9b1cdab88bff6
Anusha911 <anushamandalapu1997@gmail.com>
2022-09-06 21:50:55 +0100

    adding

--- main/main.tf
+++ main/main.tf
@@ -2,16 +2,11 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-    name = local.rg_name
-    location = var.location
-    tags = var.tags
+module "resource_group" {
+    source = "../modules/rg"
 }
-resource "azurerm_storage_account" "rg" {
-  name                     = var.storage_acc
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = var.tier
-  account_replication_type = var.replication_type 
-  tags = var.tags
-  }
+module "storage_account" {
+    source = "../modules/st"
+    depends_on = [module.resource_group]
+    resource_group_name = module.resource_group.resource_group_name
+}
